e89dae14093b451615deab344aa302832fb67039
Mark Seaman <Mark.Seaman@imac.net>
2018-12-17 15:26:10 -0700

    Lesson 1

--- unc/urls.py
+++ unc/urls.py
@@ -4,15 +4,15 @@ from views import DesignReviewForm, DesignReviews,  ReviewFeedback, ReviewReques
 
 urlpatterns = (
 
-    url(r'^review/(?P<pk>\d+)$',     DesignReviewForm.as_view()),
-    url(r'^reviews/(?P<pk>\d+)$',    DesignReviews.as_view()),
-    url(r'^review-request/(?P<pk>\d+)$', ReviewRequest.as_view()),
-    url(r'^students_350',            UncStudents350.as_view()),
-    url(r'^students',                UncStudents.as_view()),
-    url(r'^student/(?P<pk>\w+)$',    StudentTest.as_view()),
-    url(r'^feedback/(?P<pk>\w+)$',   ReviewFeedback.as_view()),
+    url(r'^review/(?P<pk>\d+)$',            DesignReviewForm.as_view()),
+    url(r'^review-request/(?P<pk>\d+)$',    ReviewRequest.as_view()),
     url(r'^review-scores/(?P<course>\w+)$', ReviewScores.as_view()),
+    url(r'^reviews/(?P<pk>\d+)$',           DesignReviews.as_view()),
+    url(r'^students_350',                   UncStudents350.as_view()),
+    url(r'^students',                       UncStudents.as_view()),
+    url(r'^student/(?P<pk>\w+)$',           StudentTest.as_view()),
+    url(r'^feedback/(?P<pk>\w+)$',          ReviewFeedback.as_view()),
 
-    url(r'^(?P<title>[\w/\-_.]*)$', UncHTML.as_view()),
+    url(r'^(?P<title>[\w/\-_.]*)$',         UncHTML.as_view()),
 
 )
