f542b7c8ef571813244a76cc1347a9ca61d6e7e9
Mark Seaman <Mark.Seaman@imac.net>
2018-12-19 16:00:42 -0700

    Fix menu on Spiritual Things

--- mybook/mybook_views.py
+++ mybook/mybook_views.py
@@ -92,7 +92,7 @@ class SpiritualSelect(RedirectView):
             title = choice(['reflect', 'teaching', 'prayers', 'bible'])
         files = listdir(join('Documents', 'spiritual', title))
         file = choice(files)
-        return '/%s/%s' % (title, file)
+        return 'https://spiritual-things.org/spiritual/%s/%s' % (title, file)
 
 
 class TabsView(MyBookDocDisplay):
