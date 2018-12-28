8fac97d18e0b981d8a952ea9bf99fd74e142134a
Mark Seaman <Mark.Seaman@imac.net>
2018-12-28 10:24:54 -0700

    Script to prep class

---
+++ test/log_test.py
@@ -0,0 +1,14 @@
+from os.path import join
+
+from hammer.settings import LOG_DIR
+
+
+def log_length_test():
+    log = join(LOG_DIR, 'hammer.log')
+    lines = open(log).read().split('\n')
+    length = len(lines)
+    if length > 2000:
+        lines = lines [-1000:]
+        open(log, 'w').write('\n'.join(lines))
+        return 'Too Long Log - %s, %s lines' % (log,length)
+    return 'Log Length OK - %s, %s lines' % (log,length)
