c32bc7e247bd6734cfbb451c4030717c42f88c88
Mark Seaman <Mark.Seaman@imac.net>
2018-12-28 12:36:32 -0700

    Update class website

--- mybook/mybook_views.py
+++ mybook/mybook_views.py
@@ -115,9 +115,9 @@ class UncDocDisplay(TemplateView):
     template_name = 'unc_doc.html'
 
     def get_context_data(self, **kwargs):
-        title = 'unc/' + self.kwargs.get('title')
-        domdoc = domain_doc(self.request.get_host(), title)
+        title = self.kwargs.get('title')
+        domdoc = domain_doc(self.request.get_host(), 'unc/' + title)
         text = doc_html_text(domdoc, '/static/images')
         site = mybook_site_title(domdoc)
         menu = main_menu(site, domdoc)
-        return dict(site=site, title=title, text=text, menu=menu)
+        return dict(site=site, title='Lesson %s' % title[-2:], text=text, menu=menu)
