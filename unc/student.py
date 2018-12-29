d9bd62c5554c4908cfcdf66ecb388ae34981e4b3
Mark Seaman <Mark.Seaman@imac.net>
2018-12-29 07:51:41 -0700

    Util function to import students

---
+++ unc/student.py
@@ -0,0 +1,10 @@
+# Import list of students
+
+
+def import_students():
+    ```
+        usage:   dj shell
+         $ from unc.student import *
+         $ import_students()
+    ```
+    print('import_students')
