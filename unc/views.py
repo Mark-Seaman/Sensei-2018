3fda07a382ed52e3021ec1b35de3acc8d2ad8ec8
Mark Seaman <Mark.Seaman@imac.net>
2019-01-06 14:25:58 -0700

    Images in slides and notes

--- unc/views.py
+++ unc/views.py
@@ -5,6 +5,10 @@ from mybook.mybook import mybook_site_title, main_menu
 from tool.document import domain_doc, doc_html_text
 
 
+def fix_images(text, image_path):
+    return text.replace('![](img/', '![](%s/' % image_path)
+
+
 class UncDocDisplay(TemplateView):
     template_name = 'unc_doc.html'
 
@@ -12,7 +16,7 @@ class UncDocDisplay(TemplateView):
         title = self.kwargs.get('title')
         doc = domain_doc(self.request.get_host(), 'unc/' + title)
         title = 'Lesson %s' % title[-2:] if title[-3:-2]=='/' else 'UNC BACS 200'
-        text = doc_html_text(doc, '/static/images')
+        text = doc_html_text(doc, '/static/images/unc/bacs200')
         site = mybook_site_title(doc)
         menu = main_menu(site, doc)
         return dict(site=site, title=title, text=text, menu=menu)
@@ -24,7 +28,7 @@ class UncSlidesDisplay(TemplateView):
     def get_context_data(self, **kwargs):
         title = self.kwargs.get('title')
         doc = 'Documents/unc' + title
-        text = read_markdown(doc)
+        text = fix_images(read_markdown(doc), '/static/images/unc/bacs200')
         bear = '\n\n---\n\n<img src="/static/images/unc/bacs200/Bear_Logo.png">\n\n---\n\n'
         return dict(markdown=bear+text+bear)
 
