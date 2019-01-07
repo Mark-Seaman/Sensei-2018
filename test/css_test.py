c9585e6573ec01fd5f89b4abffe7ba9ff05a8b33
Mark Seaman <markseaman@MacBook>
2019-01-06 19:13:03 -0700

    TIme

--- test/css_test.py
+++ test/css_test.py
@@ -2,5 +2,13 @@ from bin.shell import read_file
 from hammer.settings import BASE_DIR
 
 
-def css_test():
-    return read_file(BASE_DIR+'/static/mybook.css')
+def css_mybook_test():
+    return read_file(BASE_DIR+'/static/css/mybook.css')
+
+
+def css_unc_test():
+    return read_file(BASE_DIR+'/static/css/unc.css')
+
+
+def css_slides_test():
+    return read_file(BASE_DIR+'/static/css/slides.css')
