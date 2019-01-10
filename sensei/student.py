98814d17c93b3368b06752a197cf7d17a4a912f2
Mark Seaman <Mark.Seaman@imac.net>
2019-01-10 10:59:50 -0700

    Student dashboard

--- sensei/student.py
+++ sensei/student.py
@@ -1,6 +1,10 @@
 from csv import reader
 from traceback import format_exc
 
+from django.contrib.auth.models import User
+
+from mybook.mybook import mybook_site_title, main_menu
+
 from .models import Student
 from tool.log import log, log_exception
 from tool.user import user_add
@@ -65,3 +69,47 @@ def reading_scores():
                 log_exception()
                 print(format_exc())
     return students
+
+
+def student_scores(student_id):
+    reading = reading_scores()
+
+    return reading[17]
+
+
+def fix_images(text, image_path):
+    return text.replace('![](img/', '![](%s/' % image_path)
+
+
+def site_settings(**kwargs):
+    doc = kwargs.get('doc', 'unc/bacs200')
+    site = mybook_site_title(doc)
+    menu = main_menu(site, doc)
+    settings = dict(site=site, menu=menu)
+    settings.update(kwargs)
+    return settings
+
+
+def students():
+    return Student.objects.all()
+
+
+def student(id):
+    return Student.objects.get(pk=id)
+
+
+def register_user_domain(name, email, password, domain):
+    log('name: %s, email: %s, domain: %s' % (name, email, domain))
+    assert ' ' in name
+    first = name.split()[0]
+    last = name.split()[-1]
+    username = email
+    u = User.objects.get_or_create(username=username)[0]
+    u.first_name = first
+    u.last_name = last
+    u.email = email
+    u.is_staff = True
+    u.set_password(password)
+    u.save()
+    s = Student.objects.get_or_create(course_id=1, name=name, email=email, domain=domain)[0]
+    return s
--- sensei/views.py
+++ sensei/views.py
@@ -1,18 +1,12 @@
 from django import forms
 from django.forms import Form
 from django.views.generic import FormView, ListView, TemplateView
-from django.contrib.auth.models import User
 
 from bin.pandoc import read_markdown
-from mybook.mybook import mybook_site_title, main_menu
 from tool.document import domain_doc, doc_html_text
+
+from .student import fix_images, reading_scores, student_scores, site_settings, student, register_user_domain
 from .models import Lesson, Student
-from tool.log import log
-from .student import reading_scores
-
-
-def fix_images(text, image_path):
-    return text.replace('![](img/', '![](%s/' % image_path)
 
 
 class UncDocDisplay(TemplateView):
@@ -27,33 +21,14 @@ class UncDocDisplay(TemplateView):
         doc = domain_doc(self.request.get_host(), 'unc/' + title)
         title = 'Lesson %s' % title[-2:] if title[-3:-2]=='/' else 'UNC BACS 200'
         text = doc_html_text(doc, '/static/images/unc/bacs200')
-        site = mybook_site_title(doc)
-        menu = main_menu(site, doc)
-        reading_scores()
-        return dict(site=site, title=title, text=text, menu=menu, lessons=lessons)
-
-
-class UncSlidesDisplay(TemplateView):
-    template_name = 'unc_slides.html'
-
-    def get_context_data(self, **kwargs):
-        title = self.kwargs.get('title')
-        doc = 'Documents/unc/' + title
-        text = fix_images(read_markdown(doc), '/static/images/unc/bacs200')
-        bear = '\n\n---\n\n<img src="/static/images/unc/bacs200/Bear_Logo.png">\n\n---\n\n'
-        return dict(markdown=bear+text+bear)
-
-
-class UncRegistered(ListView):
-    template_name = 'unc_registered.html'
-    model = Student
+        return site_settings(title=title, text=text, lessons=lessons)
 
 
 class UncReading(TemplateView):
     template_name = 'unc_reading.html'
 
     def get_context_data(self, **kwargs):
-        return dict(students=reading_scores())
+        return site_settings(title='Reading Scores', students=reading_scores())
 
 
 class UncRegister(FormView):
@@ -77,18 +52,30 @@ class UncRegister(FormView):
         return super(UncRegister, self).form_valid(form)
 
 
-def register_user_domain(name, email, password, domain):
-    log('name: %s, email: %s, domain: %s' % (name, email, domain))
-    assert ' ' in name
-    first = name.split()[0]
-    last = name.split()[-1]
-    username = email
-    u = User.objects.get_or_create(username=username)[0]
-    u.first_name = first
-    u.last_name = last
-    u.email = email
-    u.is_staff = True
-    u.set_password(password)
-    u.save()
-    s = Student.objects.get_or_create(course_id=1, name=name, email=email, domain=domain)[0]
-    return s
+class UncRegistered(ListView):
+    template_name = 'unc_registered.html'
+    model = Student
+
+
+class UncSlidesDisplay(TemplateView):
+    template_name = 'unc_slides.html'
+
+    def get_context_data(self, **kwargs):
+        title = self.kwargs.get('title')
+        doc = 'Documents/unc/' + title
+        text = fix_images(read_markdown(doc), '/static/images/unc/bacs200')
+        bear = '\n\n---\n\n<img src="/static/images/unc/bacs200/Bear_Logo.png">\n\n---\n\n'
+        text = bear+text+bear
+        # return dict(markdown=text)
+        return site_settings(title=title, markdown=text)
+
+
+class UncStudent(TemplateView):
+    template_name = 'unc_student.html'
+
+    def get_context_data(self, **kwargs):
+        student_id = self.kwargs.get('id')
+        title = 'Student Dashboard'
+        reading = student_scores(student_id)
+        return site_settings(title=title, student=student(student_id), scores=reading)
+
