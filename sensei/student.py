7d3cae6f0cee2d6583c9c385156d9ca8e5ac37e2
Mark Seaman <Mark.Seaman@imac.net>
2019-01-12 11:24:39 -0700

    Use zybooks name for match

--- sensei/student.py
+++ sensei/student.py
@@ -88,9 +88,9 @@ def reading_scores():
     log('READING: ' + '   '.join(table[0]))
     scores = {}
     for row in table[1:]:
-        log('   '.join(row))
+        # log('   '.join(row))
         name = '%s' % (row[0])
-        student = Student.objects.filter(name=name)
+        student = Student.objects.filter(zbooks=name)
         if student:
             scores[student[0]] = row
         else:
