414b69cdfa1efd8653bc13bf0201b8ba1b3287c6
Mark Seaman <Mark.Seaman@imac.net>
2019-01-16 11:33:28 -0700

    Fix tests

--- test/app_test.py
+++ test/app_test.py
@@ -11,7 +11,7 @@ def app_classes_test():
 
 
 def app_functions_test():
-    return check_shell_lines('x app functions', 800, 1000)
+    return check_shell_lines('x app functions', 700, 800)
 
 
 def app_list_test():
