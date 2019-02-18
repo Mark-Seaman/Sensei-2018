50f09081a1bbd8ce9a89f2ba709857931ea40b98
Mark Seaman <Mark.Seaman@imac.net>
2019-02-18 10:29:12 -0700

    Lesson 17

--- sensei/fix.py
+++ sensei/fix.py
@@ -126,6 +126,7 @@ add_lesson ('bacs200', '13', 'Test & Debug', '2019-02-06')
 add_lesson ('bacs200', '14', 'Cascading Stylesheets', '2019-02-11')
 add_lesson ('bacs200', '15', 'CSS Color', '2019-02-13')
 add_lesson ('bacs200', '16', 'CSS Spacing & Borders', '2019-02-15')
+add_lesson ('bacs200', '17', 'Page Layout', '2019-02-18')
 
 
 #########################
