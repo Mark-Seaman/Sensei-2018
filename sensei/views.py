d7b7bca08f4ff356bccc42f49fee04af8300734b
Mark Seaman <mark.seaman@shrinking-world.com>
2019-03-18 12:38:12 -0600

    Dashboard view

--- sensei/views.py
+++ sensei/views.py
@@ -101,6 +101,17 @@ class UncRegister(FormView):
 class UncRegistered(RedirectView):
     url = '/unc/students/1'
 
+    def get_context_data(self, **kwargs):
+        student_id = self.kwargs.get('id')
+        s = Student.objects.get(pk=student_id)
+        game = UrlGame.objects.get(student=s)
+        reviews = student_reviews(student_id)
+        done = student_reviews_done(student_id)
+        feedback = review_feedback(student_id)
+        title = 'Student Dashboard'
+        return site_settings(title=title, student=student(student_id), game=game,
+                             reviews=reviews, feedback=feedback, done=done)
+
 
 class UncStudents(ListView):
     template_name = 'unc_registered.html'
