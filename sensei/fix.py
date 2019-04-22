bcd3dcfbaa2358354ad2437847cd6025eba8f9bd
Mark Seaman <Mark.Seaman@imac.net>
2019-04-22 10:33:01 -0600

    Lesson 39

--- sensei/fix.py
+++ sensei/fix.py
@@ -43,10 +43,11 @@ add_lesson ('bacs200', '34', 'Professional Image', '2019-04-08')
 add_lesson ('bacs200', '35', 'Learning Skills', '2019-04-10')
 add_lesson ('bacs200', '36', 'Non-profit Contribution', '2019-04-15')
 add_lesson ('bacs200', '37', 'Project Management', '2019-04-17')
+add_lesson ('bacs200', '38', 'Teamwork', '2019-04-19')
 
 
 from sensei.sensei import add_lesson
-add_lesson ('bacs200', '38', 'Teamwork', '2019-04-19')
+add_lesson ('bacs200', '39', 'Email Marketing', '2019-04-22')
 
 
 #########################
