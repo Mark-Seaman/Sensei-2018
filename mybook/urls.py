3db1d7ce997543b0af22a8d47b1e2ded4dbdd5d6
Mark Seaman <Mark.Seaman@imac.net>
2019-05-15 08:47:06 -0600

    Spiritual menu

--- mybook/urls.py
+++ mybook/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout
 
-from .mybook_views import BookNotes, DomainRedirect, MyBookDocDisplay, MyBookPrivateDoc, MyBookRandom, SeamansLog, SpiritualSelect
+from .mybook_views import BookNotes, DomainRedirect, MyBookDocDisplay, MyBookPrivateDoc, MyBookRandom, SeamansLog, \
+    SpiritualDoc, SpiritualSelect
 
 urlpatterns = [
 
@@ -10,13 +11,17 @@ urlpatterns = [
     url(r'^login',                              login, {'template_name': 'mybook_login.html'}, name='login'),
     url(r'^logout$',                            logout, {'next_page': '/login'}),
 
-    # Custom Pages
+    # Booknotes
     #url(r'^MarkSeaman/booknotes/(?P<title>[\w/\-.]*)$',    BookNotes.as_view()),
 
+    # Private Pages
     url(r'^info/(?P<title>[\w/\-_.]*)$',        MyBookPrivateDoc.as_view()),
 
+    # Seaman's Log
     url(r'^seamanslog$',                        SeamansLog.as_view()),
 
+    # Spiritual
+    url(r'^spiritual/Index$',                   SpiritualDoc.as_view()),
     url(r'^spiritual/(?P<title>[\w\-_.]*)$',    SpiritualSelect.as_view()),
 
     # Documents
