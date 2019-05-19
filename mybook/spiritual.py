416ebba584a9470d31e319b4bf1dbdffa26b1f84
Mark Seaman <Mark.Seaman@imac.net>
2019-05-19 08:07:54 -0600

    Simplify

--- mybook/spiritual.py
+++ mybook/spiritual.py
@@ -8,34 +8,45 @@ from tool.document import domain_doc, doc_html_text
 from tool.log import log_page
 
 
-def spiritual_topics():
-    return [('Index', 'Home'),
-            ('reflect', 'Reflect'),
-            ('bible', 'Meditate'),
-            ('teaching', 'Learn'),
-            ('walkabout', 'Journey'),
-            ('prayers', 'Pray')]
+def header_settings(site_title, logo=(None,None)):
+    return dict(title=site_title[0], subtitle=site_title[1], logo=logo[0], logo_text=logo[1])
 
 
-def spiritual_header():
-    return dict(title='Spiritual Things', subtitle='Daily Inspiration', logo=None)
-
-
-def spiritual_menu(title):
+def topic_menu(title, topics, base, home):
     def is_active(title, topic):
         return ' active' if title.startswith(topic) else ''
 
-    menu_items = [dict(url='/spiritual/'+i[0], label=i[1], active=is_active(title,i[0])) for i in spiritual_topics()]
+    menu_items = [dict(url=base+i[0], label=i[1], active=is_active(title,i[0])) for i in topics]
+    return home, menu_items
 
-    return "Spiritual Things", menu_items
 
-
-def page_settings(domain, title):
+def page_settings(domain, title, site_title, menu):
     domdoc = domain_doc(domain, title)
     text = doc_html_text(domdoc, '/static/images')
-    menu = spiritual_menu(title)
     url = join(domain, title)
-    return dict(title=title, text=text, menu=menu, url=url, header=spiritual_header())
+    header = header_settings(site_title)
+    return dict(title=title, text=text, menu=menu, url=url, header=header)
+
+
+# **********************
+
+
+def spiritual_page_settings(request, title):
+    domain = request.get_host()
+    menu = topic_menu(title, spiritual_topics(), '/spiritual/', "Spiritual Things")
+    site_title = ('Spiritual Things', 'Daily Inspiration')
+    return page_settings(domain, title, site_title, menu)
+
+    # return dict(title=title, text=text, menu=menu, url=url, header=header)
+
+
+def spiritual_topics():
+    return [('Index',       'Home'),
+            ('reflect',     'Reflect'),
+            ('bible',       'Meditate'),
+            ('teaching',    'Learn'),
+            ('walkabout',   'Journey'),
+            ('prayers',     'Pray')]
 
 
 class SpiritualDoc(TemplateView):
@@ -44,12 +55,12 @@ class SpiritualDoc(TemplateView):
     def get_context_data(self, **kwargs):
         log_page(self.request)
         title = self.kwargs.get('title', 'Index')
-        # domdoc = domain_doc(self.request.get_host(), title)
-        # text = doc_html_text(domdoc, '/static/images')
-        # menu = spiritual_menu(title)
-        # url = self.request.get_raw_uri()
-        # return dict(title=title, text=text, menu=menu, url=url, header=spiritual_header())
-        return page_settings(self.request.get_host(), title)
+        return spiritual_page_settings(self.request, title)
+
+        # page_settings(self.request.get_host(),
+        #                      title,
+        #                      ('Spiritual Things', 'Daily Inspiration'),
+        #                      spiritual_topics())
 
 
 class SpiritualSelect(RedirectView):
