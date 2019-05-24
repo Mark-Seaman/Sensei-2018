4d2e1d06606f22ba73622a43babd5d95aa0ce766
Mark Seaman <Mark.Seaman@imac.net>
2019-05-24 12:43:09 -0600

    DocRedirect

--- mybook/urls.py
+++ mybook/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # Redirect
     url(r'^$',                                  DomainRedirect.as_view()),
     url(r'^(?P<title>[\w/\-_.]*)/Missing$',     DocMissing.as_view()),
-    url(r'^redir/(?P<title>[\w/\-_.]*)$',       DocRedirect.as_view()),
+    url(r'^redirect/(?P<title>[\w/\-_.]*)$',       DocRedirect.as_view()),
 
     # Authentication
     url(r'^login',                              login, {'template_name': 'mybook_login.html'}, name='login'),
