c054e688b04a208e2412bbb291bf749cf0345a30
Mark Seaman <Mark.Seaman@imac.net>
2019-05-31 13:07:32 -0600

    Domain handling in get 9

--- mybook/views.py
+++ mybook/views.py
@@ -70,7 +70,7 @@ class DocDisplay(TemplateView):
         title = self.kwargs.get('title')
         domdoc = domain_doc(self.request.get_host(), title)
         url = doc_page(title)
-        if title != domdoc:
+        if title != domdoc and url:
             url = domain_doc(self.request.get_host(), url)
             log('REDIRECT DOMAIN: %s --> %s' % (title, domdoc))
         if url:
