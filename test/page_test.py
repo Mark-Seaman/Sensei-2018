ce9be91bc2bed71964c488b3ffc9101ccd743159
Mark Seaman <Mark.Seaman@imac.net>
2019-06-02 06:49:18 -0600

    Time

--- test/page_test.py
+++ test/page_test.py
@@ -9,11 +9,11 @@ def check_page(url, min=1, max=1):
 def page_sites_test():
     pages = [
         check_page('https://markseaman.info/Index.md',                  150, 180),
-        check_page('https://markseaman.org/MarkSeaman/Index.md',        160, 180),
+        check_page('https://markseaman.org/MarkSeaman/Index.md',        100, 110),
 
         check_page('https://seamansguide.com/guide/Index.md',           112, 130),
-        check_page('https://seamanslog.com/seamanslog/List',            124, 127),
-        check_page('https://seamanslog.com/seamanslog/Files',           124, 127),
+        check_page('https://seamanslog.com/seamanslog/List',            290, 310),
+        check_page('https://seamanslog.com/seamanslog/Files',           290, 310),
         check_page('https://seamanslog.com/seamanslog/HabitualChange',  126, 129),
 
         check_page('https://shrinking-world.com/shrinkingworld/Index',  126, 129),
