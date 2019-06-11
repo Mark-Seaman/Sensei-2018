abe0ed0f7189f2cdb5da4145d5d760d7f42d71bf
Mark Seaman <Mark.Seaman@imac.net>
2019-06-10 22:03:26 -0600

    Build word counter app

--- brain/brain.py
+++ brain/brain.py
@@ -4,6 +4,9 @@ from os import  listdir, walk
 from subprocess import Popen, PIPE
 from sys import version_info
 
+from bin.shell import read_file, word_count
+
+
 # Read the markdown document and convert it to HTML
 def doc_html(doc):
     return markdown_to_html(read_markdown(doc))
@@ -18,31 +21,61 @@ def doc_list(path):
     return [doc_entry(path, f) for f in list_files(path)]
 
 
+def count_words(directory):
+    matches = []
+    for dirname, dirnames, filenames in walk(directory):
+        for filename in filenames:
+            base = dirname.replace(directory + '/', '')
+            matches.append((filename, join(base, filename)))
+    for m in matches:
+        print('%-20s %s' % (m[0], m[1]))
+
 # Create a list of document links (doc, title)
 def doc_tree(doc):
 
     def bullet_indent(n):
         return '    ' * n + '* '
 
-    def dir_index(result, root, d, i):
+    def append_files(directory, indents):
+        file_words = 0
+        for fname in sorted(listdir(directory)):
+            if isfile(join(directory, fname)):
+                words = word_count(read_file(join(directory, fname)))
+                file_words += words
+                dname = directory.replace('Documents/', '/brain/')
+                url = join(dname, fname)
+                entry = '%s[%s](%s) - %s words\n' % (bullet_indent(indents + 1), fname, url, words)
+                result.append(entry)
+        return file_words
+
+    def append_directories(d, i):
+        for f in sorted(listdir(d)):
+            if isdir(join(d, f)):
+                dir_index(result, join(d, f), i + 1)
+
+    def dir_index(result, d, i):
         dname = d.split('/')[-1]
         url = d.replace('Documents/', '/brain/')
         result.append('%s[%s](%s)/\n' % (bullet_indent(i), dname, url))
-        for fname in sorted(listdir(d)):
-            if isfile(join(d, fname)):
-                # url = root + '--' + d + '--' + fname
-                dname = d.replace('Documents/', '/brain/')
-                url = join(dname, fname)
-                result.append('%s[%s](%s)\n' % (bullet_indent(i + 1), fname, url))
-        for f in sorted(listdir(d)):
-            if isdir(join(d, f)):
-                dir_index(result, root, join(d, f), i + 1)
+        file_words = append_files(d, i)
+        result.append('%s[%s](%s) - %s words/\n' % (bullet_indent(i), dname, url, file_words))
+
+        append_directories(d, i)
+
+        # for fname in sorted(listdir(d)):
+        #     if isfile(join(d, fname)):
+        #         # url = root + '--' + d + '--' + fname
+        #         dname = d.replace('Documents/', '/brain/')
+        #         url = join(dname, fname)
+        #         result.append('%s[%s](%s)\n' % (bullet_indent(i + 1), fname, url))
+        # for f in sorted(listdir(d)):
+        #     if isdir(join(d, f)):
+        #         dir_index(result, root, join(d, f), i + 1)
+
     result = []
     d = join('Documents', doc)
-    dir_index(result, d, d, 0)
+    dir_index(result, d, 0)
     return markdown_to_html('\n'.join(result))
-
-
 
 
 # Find the path to the requested document
