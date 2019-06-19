2cfd964d844179445ba6a089bf13bc9132f7f3e7
Mark Seaman <Mark.Seaman@imac.net>
2019-06-19 16:57:55 -0600

    Software menu

--- mybook/guide.py
+++ mybook/guide.py
@@ -3,18 +3,24 @@ from .mybook import  page_settings, page_text, topic_menu
 from .views import DocDisplay
 
 
-def seamans_guide_menu(title):
+def guide_menu(page, title):
 
-    def menu_items(title):
+    def menu_items(page):
         return [('https://shrinking-world.com', 'Shrinking World'),
                 ('https://shrinking-world.com/unc/', 'UNC Courses'),
                 ('https://shrinking-world.com/shrinkingworld/Leverage', 'Leverage'),
                 ('https://markseaman.org', 'Mark Seaman')]
 
+    return topic_menu(menu_items(title), '/seamansguide/', title)
+
+
+def guide_settings(title):
     if title.startswith('guide/Software'):
-        return topic_menu(menu_items(title), '/seamansguide/', "Software Engineering")
+        return guide_menu(title, "Software Engineering")
+    elif title.startswith('guide/PhpApps'):
+        return guide_menu(title, "PHP Apps")
     else:
-        return topic_menu(menu_items(title), '/seamansguide/', "Seaman's Guide")
+        return guide_menu(title, "Software Engineering")
 
 
 class SeamansGuide(DocDisplay):
@@ -25,5 +31,5 @@ class SeamansGuide(DocDisplay):
         site_title = "Seaman's Guides", 'Software Development Courses'
         logo = "/static/images/MarkSeaman.100.png", 'Mark Seaman'
         text = page_text(domain, title)
-        menu = seamans_guide_menu(title)
+        menu = guide_settings(title)
         return page_settings(title, site_title, logo, menu, text['text'], text['url'])
