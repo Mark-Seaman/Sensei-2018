b002110e3802906eb46c7b820ba7ca349102f019
Mark Seaman <Mark.Seaman@imac.net>
2019-06-20 07:14:55 -0600

    Guide menus and titles

--- mybook/guide.py
+++ mybook/guide.py
@@ -5,26 +5,43 @@ from .views import DocDisplay
 
 def guide_menu(page, title):
 
-    def menu_items(page):
+    def sws_items(page):
         return [('https://shrinking-world.com', 'Shrinking World'),
                 ('https://shrinking-world.com/unc/', 'UNC Courses'),
                 ('https://shrinking-world.com/shrinkingworld/Leverage', 'Leverage'),
                 ('https://markseaman.org', 'Mark Seaman')]
 
-    return topic_menu(menu_items(title), '/guide/', title)
+    def lifecycle_items(page):
+        return [('Requirements', 'Requirements'),
+                ('Design', 'Design'),
+                ('Code', 'Code'),
+                ('Test', 'Test')]
+
+    return topic_menu(lifecycle_items(title), './', title)
 
 
 def guide_settings(title):
+    logo = "/static/images/SWS_Globe_small.jpg", 'Mark Seaman'
     if title.startswith('guide/Software'):
-        return guide_menu(title, "Software Engineering")
+        site_title = "Software Engineering", 'Principles and Practices'
+        menu = guide_menu(title, "Software Engineering")
+        return site_title, menu,logo
     elif title.startswith('guide/PhpApps'):
-        return guide_menu(title, "PHP Apps")
+        site_title = "PHP Apps", 'Software Engineering Cookbook'
+        menu =  guide_menu(title, "PHP Apps")
+        return site_title, menu,logo
     elif title.startswith('guide/PythonApps'):
-        return guide_menu(title, "Python Apps")
+        site_title = "Python Apps", 'Software Engineering Cookbook'
+        menu =  guide_menu(title, "Python Apps")
+        return site_title, menu,logo
     elif title.startswith('guide/HtmlApps'):
-        return guide_menu(title, "HTML Apps")
+        site_title = "HTML Apps", 'Software Engineering Cookbook'
+        menu =  guide_menu(title, "HTML Apps")
+        return site_title, menu,logo
     else:
-        return guide_menu(title, "Seaman's Guides")
+        site_title = "Seaman's Guides", 'Software Development Courses'
+        menu =  guide_menu(title, "Seaman's Guides")
+        return site_title,menu,logo
 
 
 class SeamansGuide(DocDisplay):
@@ -32,8 +49,6 @@ class SeamansGuide(DocDisplay):
     def get_context_data(self, **kwargs):
         domain = self.request.get_host()
         title = self.request.path[1:]
-        site_title = "Seaman's Guides", 'Software Development Courses'
-        logo = "/static/images/MarkSeaman.100.png", 'Mark Seaman'
+        site_title,menu,logo = guide_settings(title)
         text = page_text(domain, title)
-        menu = guide_settings(title)
         return page_settings(title, site_title, logo, menu, text['text'], text['url'])
